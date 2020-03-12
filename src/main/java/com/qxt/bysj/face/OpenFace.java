8d548565211ebab30a4137bf51b2aeaab69d71ad
huanghz <710421059@qq.com>
2020-03-12 18:17:46 +0800

    点击事件 加入队列任务

--- src/main/java/com/qxt/bysj/face/OpenFace.java
+++ src/main/java/com/qxt/bysj/face/OpenFace.java
@@ -199,45 +199,7 @@ public class OpenFace {
         //模拟的随机数
         String orderNo = System.currentTimeMillis() + UUID.randomUUID().toString();
 
-            testThreadPoolManager.addOrders(orderNo,openId,videoId);
-
-/*        if(openId!=null && openId.length()>0){
-            User user = userService.selectByOpenid(openId);
-            Video video = videoService.selectById(videoId);
-            if(video.getHot()==null) video.setHot(0);
-            video.setHot(video.getHot()+1);
-            videoService.update(video);
-
-            Map<String, Object> query1 = new HashMap<>();
-            query1.put("videoId",dto.getVideoId());
-            List<Tag> tagList = tagService.find(query1);
-            if(tagList.size()>0){
-                for(Tag tag:tagList){
-                    if(tag.getHot()==null) tag.setHot(0);
-                    tag.setHot(tag.getHot()+1);
-                    tagService.update(tag);
-
-                    Map<String, Object> query2 = new HashMap<>();
-                    query2.put("userId",user.getId());
-                    query2.put("tagId",tag.getId());
-
-                    List<TagXuser> xuserList = tagXuserService.find(query2);
-                    if(xuserList.size()>0){
-                        for(TagXuser tagXuser:xuserList){
-                            if(tagXuser.getHot()==null) tagXuser.setHot(0);
-                            tagXuser.setHot(tagXuser.getHot()+1);
-                            tagXuserService.update(tagXuser);
-                        }
-                    }else {
-                        TagXuser obj = new TagXuser();
-                        obj.setTagid(tag.getId());
-                        obj.setUserid(user.getId());
-                        obj.setHot(1);
-                        tagXuserService.insert(obj);
-                    }
-                }
-            }
-        }*/
+        testThreadPoolManager.addOrders(orderNo,openId,videoId);
         return result;
     }
 
