41f5cb23816660f21b638d90cb09f5e1b9257e8c
qixiaotong <qixt@winningyun.com>
2020-03-31 09:41:39 +0800

    注释

--- src/main/java/com/qxt/bysj/utils/BiliRequest.java
+++ src/main/java/com/qxt/bysj/utils/BiliRequest.java
@@ -33,11 +33,11 @@ public class BiliRequest {
         httpget.setHeader("Accept-Encoding", "gzip, deflate");
         httpget.setHeader("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
         httpget.setHeader("Cache-Control", "max-age=0");
-        httpget.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36"); //这项内容很重要
+        httpget.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36"); //这项内容很重要，将爬虫伪装成浏览器
         HttpResponse response = null;
         try {
-            response = httpClient.execute(httpget);
-        } catch (ClientProtocolException e) {
+            response = httpClient.execute(httpget);//执行get请求，相当于在输入地址后敲回车键
+     } catch (ClientProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
