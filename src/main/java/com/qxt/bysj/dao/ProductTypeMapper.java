bba1b3da42cec519b4c8c15feef258fee7f27ab7
qixiaotong <qixt@winningyun.com>
2020-05-06 17:55:52 +0800

    产品分类

---
+++ src/main/java/com/qxt/bysj/dao/ProductTypeMapper.java
@@ -0,0 +1,17 @@
+package com.qxt.bysj.dao;
+
+import com.qxt.bysj.domain.ProductType;
+
+public interface ProductTypeMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ProductType record);
+
+    int insertSelective(ProductType record);
+
+    ProductType selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ProductType record);
+
+    int updateByPrimaryKey(ProductType record);
+}
---
+++ src/main/java/com/qxt/bysj/domain/ProductType.java
@@ -0,0 +1,23 @@
+package com.qxt.bysj.domain;
+
+public class ProductType {
+    private Integer id;
+
+    private String typename;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTypename() {
+        return typename;
+    }
+
+    public void setTypename(String typename) {
+        this.typename = typename == null ? null : typename.trim();
+    }
+}
