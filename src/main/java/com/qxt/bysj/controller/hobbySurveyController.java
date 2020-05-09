39c27b941fdb0cd26dc316e54365a1de11d8c64c
qixiaotong <qixt@winningyun.com>
2020-05-09 15:46:05 +0800

    喜好调查

--- src/main/java/com/qxt/bysj/controller/hobbySurveyController.java
+++ src/main/java/com/qxt/bysj/controller/hobbySurveyController.java
@@ -1,17 +1,22 @@
 package com.qxt.bysj.controller;
 
+import com.qxt.bysj.domain.Product;
 import com.qxt.bysj.domain.ProductBrand;
 import com.qxt.bysj.domain.ProductType;
 import com.qxt.bysj.service.ProductBrandService;
+import com.qxt.bysj.service.ProductService;
 import com.qxt.bysj.service.ProductTypeService;
 import com.qxt.bysj.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +24,8 @@ import java.util.Map;
 @RequestMapping(value = "/hobbySurveyPage", method = RequestMethod.POST)
 @Controller
 public class hobbySurveyController {
+    @Autowired
+    private ProductService productService;
     @Autowired
     private ProductBrandService productBrandService;
     @Autowired
@@ -50,4 +57,19 @@ public class hobbySurveyController {
         result.setData(type);
         return result;
     }
+
+    @RequestMapping(value = "/findProduct", method = RequestMethod.POST)
+    @ResponseBody
+    public Result<Object> findProduct(@RequestBody Product product, HttpServletRequest request, HttpServletResponse response, Model model) {
+        Result<Object> result = new Result<>();
+        Integer brandId = product.getBrandId();
+        Integer typeId = product.getTypeId();
+        Map<String, Object> map = new HashMap<>();
+        map.put("brandId",brandId);
+        map.put("typeId",typeId);
+        List<Product> productList = productService.find(map);
+        result.setData(productList);
+        return result;
+    }
+
 }
--- src/main/resources/static/apps/js/survey.js
+++ src/main/resources/static/apps/js/survey.js
@@ -1,48 +1,95 @@
-$("#item").click(function () {
-    // $(".item").addClass("items");
-    if($(".item").hasClass("items")){
-        $(".item").removeClass("items");
-    }else{
-        $(".item").addClass("items");
+$(function () {
+    var typeList = [];
+    var brandList = [];
+    var data = {
+        brandId: '',
+        typeId: ""
+    };
+    window.onload = function () {
+        $.ajax({
+            url: ctx + "hobbySurveyPage/findBand",
+            data: '',
+            type: "post",
+            contentType: "application/json; charset=utf-8",
+            success: function (res) {
+                console.log(res.data);
+                brandList = res.data;
+                for (var i = 0; i < brandList.length; i++) {
+                    // console.log(funList[i].id)
+                    $("#brandSelect").append("<option value='" + brandList[i].id + "'>" + brandList[i].brandname + "</option>");
+                }
+                ;
+                //使用refresh方法更新UI以匹配新状态。
+                $('#brandSelect').selectpicker('refresh');
+                //render方法强制重新渲染引导程序 - 选择ui。
+                $('#brandSelect').selectpicker('render');
+            }
+        });
+        $.ajax({
+            url: ctx + "hobbySurveyPage/findType",
+            data: '',
+            type: "post",
+            contentType: "application/json; charset=utf-8",
+            success: function (res) {
+                console.log(res.data);
+                typeList = res.data;
+                for (var i = 0; i < typeList.length; i++) {
+                    // console.log(funList[i].id)
+                    $("#typeSelect").append("<option value='" + typeList[i].id + "'>" + typeList[i].typename + "</option>");
+                }
+                ;
+                //使用refresh方法更新UI以匹配新状态。
+                $('#typeSelect').selectpicker('refresh');
+                //render方法强制重新渲染引导程序 - 选择ui。
+                $('#typeSelect').selectpicker('render');
+            }
+        });
+    };
+    $("#brandSelect").change(function (e) {
+        var options = $("#brandSelect option:selected");
+        data.brandId = options.val();
+        console.log(data.brandId, "brandId");
+        if (data.brandId && data.typeId) {
+            product()
+        }
+    });
+    $("#typeSelect").change(function (e) {
+        var options = $("#typeSelect option:selected");
+        data.typeId = options.val();
+        console.log(data.typeId, "typeId");
+        if (data.brandId && data.typeId) {
+            product()
+        }
+    });
+
+    function product() {
+        $("#productItem").empty();
+        $.ajax({
+            url: ctx + "hobbySurveyPage/findProduct",
+            data: JSON.stringify(data),
+            type: "post",
+            contentType: "application/json; charset=utf-8",
+            success: function (res) {
+                res.data.forEach(function (item){
+                    var inf = "<div  class=\"item\" id=\"item\" onclick='checkProduct(" + item.id + ")'>" +
+                        "<img src=" + item.pic + ">" +
+                        "<p class=\"name\">" +
+                        "<span>"+ item.productName+"</span>"+
+                        "</p>"+
+                        "<span class=\"price\">"+ item.price+"</span>"+
+                        "</div>"
+                    $("#productItem").empty();
+                    $("#productItem").append(inf);
+                })
+            }
+        });
     }
+    checkProduct = function (e) {
+        if ($(".item").hasClass("items")) {
+            $(".item").removeClass("items");
+        } else {
+            $(".item").addClass("items");
+        }
+        console.log(e,'id')
+    };
 });
-window.onload = function () {
-    $.ajax({
-        url: ctx + "findBand",
-        data: '',
-        type: "post",
-        contentType: "application/json; charset=utf-8",
-        success: function (res) {
-            console.log(res.data);
-            brandList = res.data;
-            for (var i = 0; i < brandList.length; i++) {
-                // console.log(funList[i].id)
-                $("#brandSelect").append("<option value='" + brandList[i].id + "'>" + brandList[i].brandname + "</option>");
-            }
-            ;
-            //使用refresh方法更新UI以匹配新状态。
-            $('#brandSelect').selectpicker('refresh');
-            //render方法强制重新渲染引导程序 - 选择ui。
-            $('#brandSelect').selectpicker('render');
-        }
-    });
-    $.ajax({
-        url: ctx + "findType",
-        data: '',
-        type: "post",
-        contentType: "application/json; charset=utf-8",
-        success: function (res) {
-            console.log(res.data);
-            typeList = res.data;
-            for (var i = 0; i < typeList.length; i++) {
-                // console.log(funList[i].id)
-                $("#typeSelect").append("<option value='" + typeList[i].id + "'>" + typeList[i].typename + "</option>");
-            }
-            ;
-            //使用refresh方法更新UI以匹配新状态。
-            $('#typeSelect').selectpicker('refresh');
-            //render方法强制重新渲染引导程序 - 选择ui。
-            $('#typeSelect').selectpicker('render');
-        }
-    });
-};
